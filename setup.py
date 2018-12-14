157a4e2286b97123a2c6112b0589e2d95bb067c6
roflcoopter <jespernilsson93@hotmail.se>
2018-12-14 18:06:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 setup(name='timebox',
-      version='0.2.2',
+      version='0.2.3',
       include_package_data=True,
       py_modules=['timebox', 'timeboximage', 'messages', 'utils/fonts', 'utils/gifreader'],
       )
