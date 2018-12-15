5594a14b8e2fd72b57d0dc004bdafe52bc7eab4a
roflcoopter <jespernilsson93@hotmail.se>
2018-12-15 16:28:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='timebox',
-      version='0.1',
+      version='0.1.1',
       package_dir = {'': 'package'},
       py_modules=['timebox', 'timeboximage', 'messages', 'utils/fonts', 'utils/gifreader'],
       data_files=[('timebox-fonts', ['examples/fonts/arcadeclassic.gif'])]
