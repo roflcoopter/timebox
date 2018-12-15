11b420bc50b763f26f026da3accb1cc0239a719b
roflcoopter <jespernilsson93@hotmail.se>
2018-12-15 14:32:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='timebox',
       version='0.2.4',
-      packages=find_packages(exclude=['tests', 'tests.*'])
+      packages=find_packages(exclude=['tests', 'tests.*']),
       include_package_data=True
       )
