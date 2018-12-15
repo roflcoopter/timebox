512bab1ffaee6affccc8df50a3cba60a991c65cf
roflcoopter <jespernilsson93@hotmail.se>
2018-12-15 14:28:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='timebox',
       version='0.2.4',
-      include_package_data=True,
-      py_modules=['timebox', 'timeboximage', 'messages', 'utils/fonts', 'utils/gifreader'],
+      packages=find_packages(exclude=['tests', 'tests.*'])
+      include_package_data=True
       )
